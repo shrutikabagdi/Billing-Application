16b7570ee9200d5bec5f8e481b5cfa59574c0288
Shrutika Bagdi <shrutikabagdi@gmail.com>
2026-01-13 15:18:20 +0530

    jdbc-connect

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS customer (
+                                        id INT PRIMARY KEY AUTO_INCREMENT,
+                                        name VARCHAR(100),
+    phone BIGINT,
+    email VARCHAR(100),
+    address VARCHAR(255)
+    );
+
